1b4b51d6f848882e004c998c4b3e9cfe43269517
uhyunjin <uhyeonjin0619@gmail.com>
2026-01-30 22:34:24 +0900

    S14P11B104-191 caffeine 의존성 추가 및 설정파일 작성 (#10)

---
+++ src/main/java/com/example/backend/global/config/CacheConfig.java
@@ -0,0 +1,31 @@
+package com.example.backend.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching // 캐시 기능 활성화
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        // 1. 사용할 캐시 이름 등록
+        cacheManager.registerCustomCache("analysis_results",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(30, TimeUnit.MINUTES) // 30분 뒤 자동 삭제 (메모리 보호)
+                        .maximumSize(1000) // 최대 1000개까지만 저장
+                        .recordStats() // 통계 기능
+                        .build()
+        );
+
+        return cacheManager;
+    }
+}
