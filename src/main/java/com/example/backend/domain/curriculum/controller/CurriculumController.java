25cfde9e4e7df2a3ed5b1c46b39c2d3a701380af
uhyunjin <uhyeonjin0619@gmail.com>
2026-01-31 23:58:30 +0900

    S14P11B104-216 커리큘럼 목록 요청 API 구현(#15)

--- src/main/java/com/example/backend/domain/curriculum/controller/CurriculumController.java
+++ src/main/java/com/example/backend/domain/curriculum/controller/CurriculumController.java
@@ -1,8 +1,9 @@
-package com.example.backend.domain.curriculum;
+package com.example.backend.domain.curriculum.controller;
 
 import com.example.backend.domain.curriculum.dto.CurriculumListResponse;
 import com.example.backend.domain.curriculum.service.CurriculumService;
 import com.example.backend.global.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Tag(name = "커리큘럼 API", description = "커리큘럼 데이터 API입니다.")
 @RestController
 @RequestMapping("/api/curriculums")
 @RequiredArgsConstructor
