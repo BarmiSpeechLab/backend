1915e95c90ba39af2b26f2f99ca2fdb91db256f5
uhyeonjin <uhyeonjin0619@gmail.com>
2026-02-04 20:45:17 +0900

    S14P11B104-249 튜티(학생)예약 조회 API 구현

--- src/main/java/com/example/backend/domain/meeting/repository/MeetingRepository.java
+++ src/main/java/com/example/backend/domain/meeting/repository/MeetingRepository.java
@@ -15,4 +15,7 @@ public interface MeetingRepository extends JpaRepository<Meeting, Long> {
     
     // 특정 튜터의 예약 가능한 일정 조회 (tutee가 null인 경우)
     List<Meeting> findByTutorIdAndTuteeIsNull(Long tutorId);
+
+    // 특정 튜티의 예약 일정 조회
+    List<Meeting> findByTuteeId(Long tuteeId);
 }
