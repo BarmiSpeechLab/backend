d5ce489f8ded0e2272176605315802aa5d48c1ec
uhyeonjin <uhyeonjin0619@gmail.com>
2026-02-04 10:39:21 +0900

    S14P11B104-249 MEETING 중복 방지 서비스 메서드 및 레포지토리 메서드 추가

--- src/main/java/com/example/backend/domain/meeting/repository/MeetingRepository.java
+++ src/main/java/com/example/backend/domain/meeting/repository/MeetingRepository.java
@@ -1,9 +1,14 @@
 package com.example.backend.domain.meeting.repository;
 
 import com.example.backend.domain.meeting.Entity.Meeting;
+import com.example.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+    // 특정 튜터가 해당 시간에 이미 생성한 미팅이 있는지 확인
+    boolean existsByTutorAndDatetime(User tutor, LocalDateTime datetime);
 }
